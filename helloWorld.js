4e879bf95969096658ac087440e58ee3f3a19460
adityakashid <adityakashid1146@gmail.com>
2023-02-20 01:05:46 +0530

    updated with the assignment 1
    updated with the assignment 1

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,21 @@
 console.log("Aditya Kashid");
+console.log(31);
+console.log("pune");
+/**
+ * 
+ */
+ var fullName ="Aditya Kashid"; 
+ console.log(fullName);
+ console.log(fullName);
+
+ var city; // variable Declaration
+ console.log(city);
+ city ="Pune"; // variable initialization
+ console.log(city);
+ city ="surat";
+ console.log("my city name is:",city);
+
+ var age = "31";
+ age = "32";
+ age = "33";
+ console.log(age);
---
+++ variable_asign_01.js
@@ -0,0 +1,20 @@
+console.log("********Step 1.********");
+
+var myFullName ;
+myFullName ="Aditya Kashid";
+console.log(myFullName);
+myFullName ="Aditya Mahesh Kashid";
+console.log(myFullName);
+
+console.log("********Step 2.********");
+var myLove = "I Love Node JS";
+console.log(myLove);
+
+console.log("********Step 3.********");
+var firstName = "Aditya";
+var lastName = "Kashid";
+var age = "22";
+var address = "Khadse lay - out............";
+var gender = "Male";
+var isMarried = "NO";
+console.log(firstName,lastName,age,address,gender,isMarried);
