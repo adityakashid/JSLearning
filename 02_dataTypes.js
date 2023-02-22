40f62adff39f90ca385d37ab2e07113e93d86922
adityakashid <adityakashid1146@gmail.com>
2023-02-22 16:12:15 +0530

    updated assignment 1,2&3
    updated assignment 1,2&3

---
+++ 02_dataTypes.js
@@ -0,0 +1,121 @@
+// string type
+var myName = "Virat Kholi";
+var fatherName = "Vikram Sharma";
+
+// integer type
+var age =31;
+var weight = 55.4;
+
+// boolen type
+var isMarried = true;
+var isLasy = false;
+
+
+var typeOfMyName = typeof myName;
+console.log("Type of this variable:", typeOfMyName);
+console.log("Type of Father Name",typeof fatherName);
+
+console.log("type age is",typeof age);
+console.log("type of weight is",typeof weight);
+
+console.log("type ismarried is",typeof isMarried);
+console.log("type of isLasy is",typeof isLasy);
+
+var graduation;
+console.log("Value of graduation variable is: ", graduation);
+console.log("Value of graduation variable is: ", typeof graduation);
+
+var city = null;
+console.log("Value of city variable is : ", city);
+console.log("Value of city variable is : ", typeof city);
+
+age = "32000";
+console.log(typeof age);
+
+
+
+console.log("************** Assignments 01 ***************");
+
+
+console.log(" STRING TYPE");
+var namee = "aditya";
+console.log(typeof namee);
+var lastName = "Kashid";
+console.log(typeof lastName);
+var location = " pune";
+console.log(typeof location);
+ 
+console.log("NUMBER TYPE");
+var rollNo = 55;
+console.log(typeof rollNo);
+var idNo = 66;
+console.log(typeof idNo);
+var bachNo = 3;
+console.log(typeof bachNo);
+
+
+console.log("BOOLEAN TYPE");
+var pass = true;
+console.log(typeof pass);
+var isOverAge = false;
+console.log(typeof isOverAge);
+var isGreat = true;
+console.log( typeof isGreat);
+
+console.log("Difference between NULL & UNDEFINED ");
+
+console.log("");
+
+console.log("null means there is no value in this variable");
+console.log("undefined shoes the value is undefined in the variable ");
+console.log("");
+
+console.log("****************** ASSIGNMENTS 02 *********************");
+
+var bankName;
+bankName = "SBI Bank";
+console.log(typeof bankName);
+var markss = "90";
+console.log(typeof markss);
+var isWorking = true;
+console.log( typeof isWorking);
+var totalCount = "Hundred and seven";
+console.log(typeof totalCount);
+var totalCount = 107;
+console.log(typeof totalCount);
+
+
+// console.log("****** swapping variable ******");
+
+// var num1 = 55;
+// var num2 = 100;
+// console.log(num1,num2);
+// var temp =num1;
+// num1 = num2;
+// num2 = temp;
+// console.log(num1,num2);
+
+console.log("********* ASSIGNMENTS 03 ********8");
+console.log("");
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("un swapped value of the variable :",sweety, cutie);
+var tempAditi = sweety;
+sweety = cutie;
+cutie = tempAditi;
+console.log("Here we swapp the value of the given variable :", sweety , cutie);
+
+
+console.log(" 02 Second question  ");
+console.log("");
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("unchange out put of the given vaiable is : ", num1,num2,num3);
+var num0 = num1;
+num1 = num2;
+num2 = num3;
+num3 = num0;
+console.log("After swapping the given variable the given out-out is : ",num1,num2,num3);
